c6cf9a00b3a806cda1cdaba957509e95a59d8987
danielaj2004 <danielajoya12@gmail.com>
2025-02-13 16:01:14 -0500

    completed task 6

--- cc_example.js
+++ cc_example.js
@@ -8,3 +8,25 @@ function findOrder(orders, orderId) {
     return orders.find(order=> order.id === orderId)
 }
 console.log(findOrder(orders, 102));
+
+// task 6
+let inventory = {
+    items: [],
+    addItem(name,quantity) {
+        this.items.push({name, quantity});
+    },
+    removeLatestItem(){
+        this.items.pop();
+    },
+    removeFirstItem(){
+        this.items.shift();
+    },
+};
+inventory.addItem("monitor", 5);
+inventory.addItem("keyboard", 10);
+inventory.addItem("Mouse", 3);
+console.log(JSON.stringify(inventory.items));
+inventory.removeFirstItem();
+console.log(JSON.stringify(inventory.items));
+inventory.removeFirstItem();
+console.log(JSON.stringify(inventory.items));
